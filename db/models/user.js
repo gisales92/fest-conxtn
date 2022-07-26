b439e77568796c1d02bbe2b45936e3240a478ed2
gisales92 <isales.greg@gmail.com>
2022-07-25 20:22:50 -0700

    created user model

--- db/models/user.js
+++ db/models/user.js
@@ -1,35 +1,78 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    email: {
-      allowNull: false,
-      type: DataTypes.STRING,
-      validates: {
-        isEmail: true,
-        len: [3, 255],
-      }
-    },
-    username: {
-      allowNull: false,
-      type: DataTypes.STRING,
-      validates: {
-        len: [1, 255],
-      },
-    },
-    hashedPassword: {
-      allowNull: false,
-      type: DataTypes.STRING.BINARY,
-      validates: {
-        len: [60, 60],
-      },
-    },
-    tokenId: {
-      type: DataTypes.STRING
-    }
-  }, {});
+"use strict";
+const { Validator } = require("sequelize");
 
-  User.associate = function(models) {
-  };
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true,
+        validate: {
+          notNull: true,
+          len: [3, 255],
+          isEmail: true,
+          isLowercase: true,
+        },
+      },
+      username: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true,
+        validate: {
+          notNull: true,
+          len: [1, 255],
+          isNotEmail(value) {
+            if (Validator.isEmail(value)) {
+              throw new Error("Username cannot be an email.");
+            }
+          },
+        },
+      },
+      firstName: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        validate: {
+          notNull: true,
+          len: [1, 20],
+        },
+      },
+      lastName: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        validate: {
+          notNull: true,
+          len: [1, 30],
+        },
+      },
+      profilePicUrl: {
+        allowNull: true,
+        type: DataTypes.STRING,
+      },
+      city: {
+        allowNull: true,
+        type: DataTypes.STRING,
+      },
+      state: {
+        allowNull: true,
+        type: DataTypes.STRING,
+      },
+      hashedPassword: {
+        allowNull: false,
+        type: DataTypes.STRING.BINARY,
+        validate: {
+          len: [60, 60],
+        },
+      },
+      tokenId: {
+        type: DataTypes.STRING,
+      },
+    },
+    {}
+  );
+
+  User.associate = function (models) {};
 
   return User;
 };
