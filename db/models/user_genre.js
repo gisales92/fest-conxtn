68b3291e0e4428f6d7cf8102fa765b8737cff587
gisales92 <isales.greg@gmail.com>
2022-07-26 20:22:32 -0700

    Created user_genre model

--- db/models/user_genre.js
+++ db/models/user_genre.js
@@ -1,10 +1,26 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const User_Genre = sequelize.define('User_Genre', {
-    userId: DataTypes.INTEGER,
-    genreId: DataTypes.INTEGER
-  }, {});
-  User_Genre.associate = function(models) {
+  const User_Genre = sequelize.define(
+    "User_Genre",
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          notNull: true,
+        },
+      },
+      genreId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          notNull: true,
+        },
+      },
+    },
+    {}
+  );
+  User_Genre.associate = function (models) {
     // associations can be defined here
   };
   return User_Genre;
