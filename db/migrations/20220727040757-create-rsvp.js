eab22aee4ae62e0ff2f2593601db5e5916bd0776
gisales92 <isales.greg@gmail.com>
2022-07-26 21:11:19 -0700

    Created rsvp migration

--- db/migrations/20220727040757-create-rsvp.js
+++ db/migrations/20220727040757-create-rsvp.js
@@ -1,27 +1,30 @@
-'use strict';
+"use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('RSVPs', {
+    return queryInterface.createTable("RSVPs", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       type: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now"),
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now"),
+      },
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('RSVPs');
-  }
+    return queryInterface.dropTable("RSVPs");
+  },
 };
