ef3f0afe312be72fa66401bdfcb3320d2888ac29
gisales92 <isales.greg@gmail.com>
2022-07-27 20:18:43 -0700

    Added messages seeders for demo user

---
+++ db/seeders/20220728025820-messages.js
@@ -0,0 +1,28 @@
+"use strict";
+
+function r(o) {
+  o.createdAt = new Date();
+  o.updatedAt = new Date();
+  return o;
+}
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("Messages", [
+      r({
+        senderId: 2,
+        recipientId: 1,
+        body: "Hey! Do you want to join my crew?",
+      }),
+      r({
+        senderId: 1,
+        recipientId: 4,
+        body: "Are you cr1TiKal from YouTube?",
+      }),
+    ]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Messages");
+  },
+};
