f018bc9431800b330ed6f8106c3c133c8e4b0c5e
gisales92 <isales.greg@gmail.com>
2022-07-27 17:56:37 -0700

    Added reply model and associations

---
+++ db/models/reply.js
@@ -0,0 +1,36 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const Reply = sequelize.define(
+    "Reply",
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          notNull: true,
+        },
+      },
+      postId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          notNull: true,
+        },
+      },
+      body: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: true,
+          notEmpty: true,
+        },
+      },
+    },
+    {}
+  );
+  Reply.associate = function (models) {
+    Reply.belongsTo(models.User);
+    Reply.belongsTo(models.Post);
+  };
+  return Reply;
+};
