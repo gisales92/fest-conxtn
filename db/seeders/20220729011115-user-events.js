3ca9671eb6a2d4ca57d3823e63318ebb49d92f48
gisales92 <isales.greg@gmail.com>
2022-07-28 18:48:10 -0700

    Finished adding user_event seed data

--- db/seeders/20220729011115-user-events.js
+++ db/seeders/20220729011115-user-events.js
@@ -11,48 +11,108 @@ module.exports = {
     return queryInterface.bulkInsert("User_Events", [
       r({
         userId: 1,
-        eventId: 3,
+        eventId: 22,
         rsvpId: 1,
       }),
       r({
         userId: 1,
-        eventId: 3,
+        eventId: 24,
         rsvpId: 1,
       }),
       r({
         userId: 1,
-        eventId: 3,
+        eventId: 20,
         rsvpId: 1,
       }),
       r({
         userId: 1,
-        eventId: 3,
-        rsvpId: 1,
+        eventId: 14,
+        rsvpId: 2,
       }),
       r({
         userId: 1,
-        eventId: 3,
+        eventId: 15,
+        rsvpId: 2,
+      }),
+      r({
+        userId: 2,
+        eventId: 4,
         rsvpId: 1,
       }),
       r({
-        userId: 1,
-        eventId: 3,
+        userId: 2,
+        eventId: 9,
+        rsvpId: 2,
+      }),
+      r({
+        userId: 2,
+        eventId: 15,
+        rsvpId: 2,
+      }),
+      r({
+        userId: 2,
+        eventId: 21,
+        rsvpId: 2,
+      }),
+      r({
+        userId: 3,
+        eventId: 1,
         rsvpId: 1,
       }),
       r({
-        userId: 1,
-        eventId: 3,
+        userId: 3,
+        eventId: 10,
+        rsvpId: 2,
+      }),
+      r({
+        userId: 3,
+        eventId: 16,
+        rsvpId: 2,
+      }),
+      r({
+        userId: 3,
+        eventId: 22,
+        rsvpId: 2,
+      }),
+      r({
+        userId: 4,
+        eventId: 5,
         rsvpId: 1,
       }),
       r({
-        userId: 1,
-        eventId: 3,
+        userId: 4,
+        eventId: 11,
+        rsvpId: 2,
+      }),
+      r({
+        userId: 4,
+        eventId: 16,
+        rsvpId: 2,
+      }),
+      r({
+        userId: 4,
+        eventId: 23,
+        rsvpId: 2,
+      }),
+      r({
+        userId: 5,
+        eventId: 2,
         rsvpId: 1,
       }),
       r({
-        userId: 1,
-        eventId: 3,
-        rsvpId: 1,
+        userId: 5,
+        eventId: 12,
+        rsvpId: 2,
+      }),
+      r({
+        userId: 5,
+        eventId: 17,
+        rsvpId: 2,
+      }),
+      r({
+        userId: 5,
+        eventId:3,
+        rsvpId: 2,
       }),
     ]);
   },
