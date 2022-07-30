ad61503f7fba091cddf9faa159297e09af23e02c
gisales92 <isales.greg@gmail.com>
2022-07-29 19:32:03 -0700

    Added handlevalidationerrors util

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,3 +1,82 @@
-const express = require('express');
-const asyncHandler = require('express-async-handler');
+const express = require("express");
+const asyncHandler = require("express-async-handler");
 const { check } = require("express-validator");
+
+const {
+  setTokenCookie,
+  restoreUser,
+  requireAuth,
+} = require("../../utils/auth");
+const { User } = require("../../db/models");
+const { handleValidationErrors } = require("../../utils/validation");
+const router = express.Router();
+
+const validateLogin = [
+  check("credential") // can check credential to check both email and username
+    .exists({ checkFalsy: true })
+    .withMessage("A valid email or username is required")
+    .notEmpty()
+    .withMessage("A valid email or username is required")
+    .isEmail()
+    .withMessage("A valid email or username is required"),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Password is required"),
+  handleValidationErrors,
+];
+
+// Log in
+router.post(
+  "/",
+  validateLogin,
+  asyncHandler(async (req, res, next) => {
+    const { credential, password } = req.body;
+
+    const user = await User.login(credential, password);
+
+    if (!user) {
+      const err = new Error("Invalid credentials");
+      err.status = 401;
+      err.title = "Login failed";
+      err.errors = ["The provided credentials were invalid."];
+      next(err);
+      return err;
+    }
+
+    const token = await setTokenCookie(res, user);
+
+    return res.json({
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      token: token,
+    });
+  })
+);
+
+// Restore session user
+router.get("/", requireAuth, (req, res, next) => {
+  const { user } = req;
+  if (user) {
+    return res.json({
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      token: req.cookies.token,
+    });
+  } else {
+    next();
+    // res.status(401);
+    // return res.json({});
+  }
+});
+
+// Log out
+router.delete("/", (_req, res) => {
+  res.clearCookie("token");
+  return res.json({ message: "success" });
+});
+
+module.exports = router;
