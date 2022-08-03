32a529e12545da6c51d09062abba483e1130990b
gisales92 <isales.greg@gmail.com>
2022-08-02 19:39:59 -0700

    Finished get replies to a post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
 
-const { Post, Reply } = require("../../db/models");
+const { Post, Reply, User } = require("../../db/models");
 const { requireAuth } = require("../../utils/auth");
 
 const router = express.Router();
@@ -11,6 +11,37 @@ router.get(
   "/:postId/replies",
   asyncHandler(async function (req, res, next) {
     const postId = req.params.postId;
+
+    const post = await Post.findByPk(postId, {
+      include: {
+        model: Reply,
+        include: {
+            model: User,
+            attributes: ["id", "username", "profilePicUrl"]
+        }
+      },
+    });
+    if (!post) {
+      res.status(404);
+      return res.json({
+        message: "Unable to find a Post with that ID",
+        statusCode: 404,
+      });
+    }
+    // remove extra data from replies and the og post
+    const postReplies = post.Replies;
+    const replies = [];
+    postReplies.forEach((replyObj) => {
+        const rep = {};
+        rep.id = replyObj.id;
+        rep.user = replyObj.User;
+        rep.postId = replyObj.postId;
+        rep.body = replyObj.body;
+        rep.time = replyObj.createdAt;
+        replies.push(rep)
+    })
+    res.status(200);
+    res.json({ replies });
   })
 );
 
