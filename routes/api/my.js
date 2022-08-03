6b835bc5483d445ef4367c9cd9ebff34a6640c2a
gisales92 <isales.greg@gmail.com>
2022-08-02 20:38:52 -0700

    created the get my/genres route

--- routes/api/my.js
+++ routes/api/my.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
 
-const { Post, Reply, User } = require("../../db/models");
+const { Post, Reply, User, Genre } = require("../../db/models");
 const { requireAuth } = require("../../utils/auth");
 const { validateReply } = require("../../utils/validation");
 
@@ -12,8 +12,21 @@ router.get(
   "/genres",
   requireAuth,
   asyncHandler(async function (req, res, next) {
-
-
+    // grab userId from the req.user object, use it to retrieve that user's record and include their genres
+    const userId = req.user.id;
+    const user = await User.findByPk(userId, {
+        include: Genre,
+      });
+      const genres = user.Genres;
+      // got to remove extra data like createdAt, updatedAt, and the user_genre join table info
+      userGenres = genres.map((genreObj) => {
+        const genre = {};
+        genre.id = genreObj.id;
+        genre.type = genreObj.type;
+        return genre;
+      });
+      res.status(200);
+      return res.json({ genres: userGenres });
   })
 );
 
