8a9e98434cd80bae89bdb20ab1a3f8fa781e3bac
gisales92 <isales.greg@gmail.com>
2022-08-02 19:42:28 -0700

    Set up post a reply route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -45,4 +45,25 @@ router.get(
   })
 );
 
+// post a reply to a post specified by its postId
+router.post(
+    "/:postId/replies",
+    asyncHandler(async function (req, res, next) {
+      const postId = req.params.postId;
+
+      const post = await Post.findByPk(postId);
+      if (!post) {
+        res.status(404);
+        return res.json({
+          message: "Unable to find a Post with that ID",
+          statusCode: 404,
+        });
+      }
+
+      res.status(200);
+      res.json({ post });
+    })
+  );
+
+
 module.exports = router;
