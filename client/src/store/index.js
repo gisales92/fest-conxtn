eaf619e815f9f1e253c82476ede9e68d748b3b3d
gisales92 <isales.greg@gmail.com>
2022-08-03 21:16:22 -0700

    Finished store to support model component

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,16 +1,17 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import ui from "./ui";
 
 const rootReducer = combineReducers({
-    // import reducers and place here
-  });
+  ui,
+});
 
 let enhancer;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   enhancer = applyMiddleware(thunk);
 } else {
-  const logger = require('redux-logger').default;
+  const logger = require("redux-logger").default;
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
