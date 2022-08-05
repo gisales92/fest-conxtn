1df18bc707f52ca52943503efa2705c12a602233
gisales92 <isales.greg@gmail.com>
2022-08-04 20:17:26 -0700

    added get events by genre to event store

--- client/src/store/events.js
+++ client/src/store/events.js
@@ -1,50 +1,74 @@
 // constants
 export const SET_EVENTS = "events/SET_EVENTS";
+export const SET_GENRE_EVENTS = "events/SET_GENRE_EVENTS";
 
 // selectors
 export const allEventsSelector = (state) => state.events.all;
+export const eventByIdSelector = (id) => (state) => state.events.all[id];
+export const genreEventsSelector = (state) => state.events.genre;
 
 // action creators
 // set all events
 export function setAllEvents(events) {
-    return {
-        type: SET_EVENTS,
-        events,
-    };
+  return {
+    type: SET_EVENTS,
+    events,
+  };
+}
+// set genre events
+export function setGenreEvents(events) {
+  return {
+    type: SET_GENRE_EVENTS,
+    events,
+  };
 }
 
 // thunks
 // fetch all events thunk
 export const fetchAllEvents = () => async (dispatch) => {
-    const res = await fetch("/api/events");
-    const data = await res.json();
+  const res = await fetch("/api/events");
+  const data = await res.json();
 
-    dispatch(setAllEvents(data.events));
-    return data;
-}
+  dispatch(setAllEvents(data.events));
+  return data;
+};
+// fetch events of a genre thunk
+export const fetchGenreEvents = (genreId) => async (dispatch) => {
+  const res = await fetch(`/api/events?genre=${genreId}`);
+  const data = await res.json();
+
+  dispatch(setGenreEvents(data.events));
+  return data;
+};
 
 // reducer
-export default function eventsReducer (
-    state = { all: {}, genre: {}, current: {}, user: {} },
-    action
+export default function eventsReducer(
+  state = { all: {}, genre: {}, current: {}, user: {} },
+  action
 ) {
-    const newState = { ...state };
-    newState.all = { ...state.all };
-    newState.genre = { ...state.genre };
-    newState.current = { ...state.current };
-    newState.user = { ...state.user };
+  const newState = { ...state };
+  newState.all = { ...state.all };
+  newState.genre = { ...state.genre };
+  newState.current = { ...state.current };
+  newState.user = { ...state.user };
 
-    switch (action.type) {
-        case SET_EVENTS:
-            const events = {};
-            action.events.forEach((event) => {
-                events[event.id] = event;
-            });
-            newState.all = events;
-            break
-
-        default:
-            break;
-    }
-    return newState;
+  switch (action.type) {
+    case SET_EVENTS:
+      const events = {};
+      action.events.forEach((event) => {
+        events[event.id] = event;
+      });
+      newState.all = events;
+      break;
+    case SET_GENRE_EVENTS:
+      const gEvents = {};
+      action.events.forEach((event) => {
+        gEvents[event.id] = event;
+      });
+      newState.genre = gEvents;
+      break;
+    default:
+      break;
+  }
+  return newState;
 }
