bc2e4c1391e559f6c297388ea03957708f725411
gisales92 <isales.greg@gmail.com>
2022-08-08 16:51:00 -0700

    Added modals and navbar to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,59 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
-
-import UserList from './components/UsersList';
-
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import NavBar from "./components/navigation/NavBar";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import LoginPage from "./components/auth/LoginPage";
+import SignupPage from "./components/auth/SignupPage";
+import { authenticate } from "./store/session";
+import { modalSelector } from "./store/ui";
+import Modal from "./components/modals/Modal";
 
 function App() {
+  const [loaded, setLoaded] = useState(false);
+  const dispatch = useDispatch();
+
+  const modal = useSelector(modalSelector);
+
+  useEffect(() => {
+    if (!loaded) {
+      (async () => {
+        try {
+          await dispatch(authenticate());
+        } finally {
+          setLoaded(true);
+        }
+      })();
+    }
+  }, [dispatch, modal, loaded]);
 
   return (
-    <BrowserRouter>
-        <nav>
-            <ul>
-                <li><NavLink to="/" activeClass="active">Home</NavLink></li>
-                <li><NavLink to="/users" activeClass="active">Users</NavLink></li>
-            </ul>
-        </nav>
-        <Switch>
-            <Route path="/users">
-                <UserList />
+    <div>
+      {modal && <Modal />}
+      <BrowserRouter>
+        <NavBar />
+
+        {loaded ? (
+          <Switch>
+            <Route path="/" exact={true}>
+              Home Page Component
             </Route>
 
-            <Route path="/">
-                <h1>My Home Page</h1>
+            <Route path="/login" exact={true}>
+              <LoginPage />
             </Route>
-        </Switch>
-    </BrowserRouter>
+
+            <Route path="/signup" exact={true}>
+              <SignupPage />
+            </Route>
+          </Switch>
+        ) : (
+          <div className="fill-screen center-content">
+            Loading...
+          </div>
+        )}
+      </BrowserRouter>
+    </div>
   );
 }
 
