88f7dcf051a19cfa44e1ae743b0fb30e9bf7870f
gisales92 <isales.greg@gmail.com>
2022-08-09 03:44:50 -0700

    added infinite scroll to event list

--- client/src/components/events/EventList.js
+++ client/src/components/events/EventList.js
@@ -1,19 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { allEventsSelector } from "../../store/events";
 import EventCard from "./EventCard";
 import "../../styles/eventCard.css";
 
 function EventList() {
-    const events = useSelector(allEventsSelector);
-    const eventCards = Object.values(events).map(event => <EventCard key={event.id} event={event} />)
+  const [eventKeys, setEventKeys] = useState([1, 2, 3, 4, 5, 6]);
+  const events = useSelector(allEventsSelector);
+  const eventCards = eventKeys.map((key) => (
+    <EventCard key={key} event={events[key]} />
+  ));
 
-    return (
-        <div className="event-list-div">
-            <h1 className="event-list-header">Events</h1>
-            <ul className="event-list-ul">{eventCards}</ul>
-        </div>
-    );
-};
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop !==
+      document.documentElement.offsetHeight
+    ) {
+      return;
+    } else {
+      const newKeys = [...eventKeys];
+      const startingKey = eventKeys.length + 1;
+      for (let i = startingKey; i < startingKey + 6; i++) {
+        if (events[i]) {
+          newKeys.push(i);
+        } else {
+          setEventKeys(newKeys);
+        }
+      }
+      setEventKeys(newKeys);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [eventKeys]);
+
+  return (
+    <div className="event-list-div">
+      <h1 className="event-list-header">Events</h1>
+      <ul className="event-list-ul">{eventCards}</ul>
+    </div>
+  );
+}
 
 export default EventList;
