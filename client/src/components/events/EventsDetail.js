8343a205709c5c94f8e5843f3edf85ed3aea8970
gisales92 <isales.greg@gmail.com>
2022-08-08 20:07:05 -0700

    started on event detail page

---
+++ client/src/components/events/EventsDetail.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from "react";
+import { useRouteMatch } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import * as eventActions from "../../store/events";
+import * as postActions from "../../store/posts";
+import * as replyActions from "../../store/replies";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function EventDetail() {
+  const dispatch = useDispatch();
+  const [updated, setUpdated] = useState(false);
+  const match = useRouteMatch({
+    path: "/events/:url",
+    exact: true,
+  });
+  const url = match.params.url;
+
+  useEffect(() => {
+    if (!updated && url !== undefined) {
+      setUpdated(true);
+    }
+  }, [dispatch, updated, url]);
+
+  const restaurant = useSelector(restaurantActions.restaurantUrlSelector(url));
+  const reviews = useSelector(restaurantReviewsSelector);
+
+  return (
+    <div className="restDetail">
+      <img
+        src={`${restaurant?.preview_image_url}`}
+        alt="Restaurant Header"
+        className="restHeaderImg"
+        crossOrigin=""
+      />
+      <div className="restProfile">
+        <h1 className="restName">{restaurant?.name}</h1>
+        <div className="restOverview">
+          {restaurant?.rating ? (
+            <p className="restRating">
+              Rating: {restaurant?.rating.toFixed(2)}/5
+            </p>
+          ) : null}
+          <p className="restInfo">
+            {restaurant?.cuisine_type} • {"$".repeat(restaurant?.price)} •{" "}
+            {restaurant?.location.city}
+          </p>
+        </div>
+        {restaurant ? (
+          <button
+            className="nav-button hover-effect sign-up-button reservation-button"
+            onClick={openReservationModal}
+          >
+            <span>Make A Reservation</span>
+            <FontAwesomeIcon icon={faClipboard} className="icon" />
+          </button>
+        ) : null}
+        {restaurant ? (
+          <div className="restDetails">
+            <h4 className="rest-detail-header">Additional Info</h4>
+            <h6 className="rest-detail-title">Opening Time</h6>
+            <p className="rest-detail-info">
+              {fixTimes(restaurant.opening_time)}
+            </p>
+            <h6 className="rest-detail-title">Closing Time</h6>
+            <p className="rest-detail-info">
+              {fixTimes(restaurant.closing_time)}
+            </p>
+            <h6 className="rest-detail-title">Location</h6>
+            <p className="rest-detail-info">{restaurant.address_line_1}</p>
+            <p className="rest-detail-info">{restaurant.address_line_2}</p>
+            <p className="rest-detail-info">{`${restaurant.location?.city}, ${restaurant.location?.state}`}</p>
+          </div>
+        ) : null}
+        <div className="restaurant-gallery">
+          <RestaurantGallery props={{ updated, restaurant }} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default EventDetail;
