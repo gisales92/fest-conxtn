0a8af14efea2f44203a7f85d2e99411942e11b8d
gisales92 <isales.greg@gmail.com>
2022-08-10 18:46:46 -0700

    got rsvp buttons to show current rsvp selection

--- client/src/components/events/RSVPBar.js
+++ client/src/components/events/RSVPBar.js
@@ -1,17 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import * as eventActions from "../../store/events";
 
 const RSVPBar = ({ rsvpId }) => {
-    console.log(rsvpId)
   const [rsvp, setRSVP] = useState(rsvpId);
+
+  useEffect(() => {
+      const el = document.querySelector(".b1");
+      const el1 = document.querySelector(".b2");
+    if (rsvp === 1) {
+        el.classList.add("active");
+        el1.classList.remove("active");
+    } else if (rsvp === 2) {
+        el1.classList.add("active");
+        el.classList.remove("active");
+    } else {
+        el.classList.remove("active");
+        el1.classList.remove("active");
+    }
+  }, [rsvp])
+
   return (
     <div className="rsvp-bar-outer">
       <div className="rsvp-bar-first">
-        <span>RSVP here:</span>
+        <span>RSVP:</span>
       </div>
-      <button className="going-rsvp-button">Going</button>
-      <button className="interested-rsvp-button">Interested</button>
-      {rsvp ? <button className="clear-rsvp-button">Clear</button> : null}
+      <button className="b1 rsvp-bar">Going</button>
+      <button className="b2 rsvp-bar">Interested</button>
+      {rsvp ? <button className="b3 rsvp-bar">Clear</button> : null}
     </div>
   );
 };
