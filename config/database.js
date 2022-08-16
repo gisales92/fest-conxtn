9b658ccf4da20b1de909c5d5fa45d8a8e991daa9
gisales92 <isales.greg@gmail.com>
2022-08-15 18:00:48 -0700

    updated production vars

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-const config = require('./');
+const config = require("./");
 const db = config.db;
 const username = db.username;
 const password = db.password;
@@ -11,18 +11,24 @@ module.exports = {
     password,
     database,
     host,
-    dialect: 'postgres',
-    seederStorage: 'sequelize',
+    dialect: "postgres",
+    seederStorage: "sequelize",
   },
   test: {
     dialect: "sqlite",
     DB_CONN: "sqlite.memory",
     logging: false,
-    seederStorage: 'sequelize',
+    seederStorage: "sequelize",
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    seederStorage: 'sequelize',
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    seederStorage: "sequelize",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
