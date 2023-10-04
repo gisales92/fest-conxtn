b2f7ffcfadd75475f39a07b06788c254b55e698e
gisales92 <isales.greg@gmail.com>
2023-10-03 22:19:16 -0700

    removed dev csrf route

--- routes/index.js
+++ routes/index.js
@@ -28,12 +28,4 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-      'XSRF-Token': csrfToken
-    });
-  });
-
 module.exports = router;
